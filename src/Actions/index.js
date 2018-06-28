07c28ae07acaa530e4bb6f1d64a9a4ebf3c7c12c
ziv-zhao <ziv.zhao@perficient.com>
2018-06-28 11:01:41 +0800

    fix eslint issue

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,23 +1,23 @@
 export const addPost = ({ data }) => ({
-    type: "ADD_POST",
-    data: {
-        editing: false,
-        ...data
-    }
+	type: 'ADD_POST',
+	data: {
+		editing: false,
+		...data
+	}
 });
 
 export const deletePost = ({ id }) => ({
-    type: "DELETE_POST",
-    id,
+	type: 'DELETE_POST',
+	id,
 });
 
 export const editPost = ({ id }) => ({
-    type: "EDIT_POST",
-    id
+	type: 'EDIT_POST',
+	id
 });
 
 export const updatePost = ({ id, data }) => ({
-    type: "UPDATE_POST",
-    id,
-    data
+	type: 'UPDATE_POST',
+	id,
+	data
 });
--- src/Reducers/postReducer.js
+++ src/Reducers/postReducer.js
@@ -1,27 +1,27 @@
 const postReducer = (state = [], action) => {
-  switch (action.type) {
-    case "ADD_POST":
-      return state.concat([{...action.data, id: state.length+1}]);
-    case "DELETE_POST":
-      return state.filter(post => post.id !== action.id);
-    case "EDIT_POST":
-      return state.map(
-        post =>
-          post.id === action.id ? { ...post, editing: !post.editing } : post
-      );
-    case "UPDATE_POST":
-      return state.map(post => {
-        if (post.id === action.id) {
-          return {
-            ...post,
-            title: action.data.title,
-            message: action.data.message,
-            editing: !post.editing
-          };
-        } else return post;
-      });
-    default:
-      return state;
-  }
+	switch (action.type) {
+	case 'ADD_POST':
+		return state.concat([{...action.data, id: state.length+1}]);
+	case 'DELETE_POST':
+		return state.filter(post => post.id !== action.id);
+	case 'EDIT_POST':
+		return state.map(
+			post =>
+				post.id === action.id ? { ...post, editing: !post.editing } : post
+		);
+	case 'UPDATE_POST':
+		return state.map(post => {
+			if (post.id === action.id) {
+				return {
+					...post,
+					title: action.data.title,
+					message: action.data.message,
+					editing: !post.editing
+				};
+			} else return post;
+		});
+	default:
+		return state;
+	}
 };
 export default postReducer;
