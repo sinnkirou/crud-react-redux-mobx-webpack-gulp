a1495b33b2552aecab1e7b1120eb69aba9692e85
ziv-zhao <ziv.zhao@perficient.com>
2018-07-09 14:48:08 +0800

    remove useless code

--- index.js
+++ index.js
@@ -1,38 +1,143 @@
+import cookieParser from "cookie-parser";
+import createError from "http-errors";
 import express from "express";
+import http from "http";
+import logger from "morgan";
 import path from "path";
 import ssr from "./build/views/ssr";
+var debug = require("debug")("crud-react-redux:server"); // eslint-disable-line no-undef
 
 const app = express();
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 // Serving static files
 app.use("/build", express.static(path.resolve(__dirname, "build")));
-app.use(express.static("src/public"));
+app.use(express.static(path.join(__dirname, "src/public")));
 
 // view engine setup
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "pug");
 
-// hide powered by express
-app.disable("x-powered-by");
-// start the server
-app.listen(process.env.PORT || 3000); // eslint-disable-line no-undef
-
 // server rendered home page
 app.get("/", (req, res) => {
-	const { preloadedState, content}  = ssr();
-	res.render("index", {title: "Server Rendered Page", preloadedState, content});
+	const { preloadedState, content } = ssr();
+	res.render("index", {
+		title: "Server Rendered Page",
+		preloadedState,
+		content
+	});
 });
 
 // Pure client side rendered page
 app.get("/client", (req, res) => {
-	res.render("index", {title: "Client Side Rendered page"});
+	res.render("index", { title: "Client Rendered page" });
 });
 
 // stop server during local development
 app.get("/exit", (req, res) => {
-	if(process.env.PORT) { // eslint-disable-line no-undef
+	if (process.env.PORT) { // eslint-disable-line no-undef
 		res.send("Sorry, the server denies your request");
 	} else {
 		res.send("shutting down");
 		process.exit(0); // eslint-disable-line no-undef
 	}
 });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
+});
+
+// hide powered by express
+app.disable("x-powered-by");
+
+/**
+ * Get port from environment and store in Express.
+ */
+var port = normalizePort(process.env.PORT || "3000");// eslint-disable-line no-undef
+app.set("port", port);
+
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+	var port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+
+	return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+	if (error.syscall !== "listen") {
+		throw error;
+	}
+
+	var bind = typeof port === "string"
+		? "Pipe " + port
+		: "Port " + port;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+	case "EACCES":
+		console.error(bind + " requires elevated privileges"); // eslint-disable-line no-console
+		process.exit(1); // eslint-disable-line no-undef
+		break;
+	case "EADDRINUSE":
+		console.error(bind + " is already in use"); // eslint-disable-line no-console
+		process.exit(1); // eslint-disable-line no-undef
+		break;
+	default:
+		throw error;
+	}
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+	var addr = server.address();
+	var bind = typeof addr === "string"
+		? "pipe " + addr
+		: "port " + addr.port;
+	debug("Listening on " + bind);
+}
